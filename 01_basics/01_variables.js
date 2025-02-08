8dcc3a8641688681cecff0583a7ca03945e554ff
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-02-08 11:17:49 +0000

    Pending changes exported from your codespace

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 213
+let accountEmail = "test@test.com"
+
+accountCity = "Jersey"
+
+console.log("testaa")
+
+console.log(accountId);
+console.table([accountEmail,accountId, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,4 @@
+"use strict"; //Treat all JS for newer version
+
+// alert(3-1)
+console.log(3+3)
