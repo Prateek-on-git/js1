141cb41781f82025f841ea684f43bb893cf88590
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-02-08 13:45:42 +0000

    sd

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,16 @@
 let name = "hitesh"
 let age = 90
 let isLoggedIn = false
+
+//number
+/*
+bigint
+string
+boolean ->true false
+null -> standalone value
+undefined
+symbol - unique
+
+object */
+
+console.log(typeof age);
