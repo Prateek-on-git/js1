335752d811223e3fe8940b656edc4e0f9ffd878f
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-02-11 08:08:16 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+//Primitive & non Primitive
+
+// 7 types of Primitive (Call by Value) : String,Number, Boolean, Null,
+// Undefined, Symbol, BigInt 
+
+
+// Symbol - const id =Symbol('223')
+//Reference (Non Primitive)
+//Array, Objects, Functions
+
+
+const id = Symbol( '123' )
+const ida = Symbol('123')
+
+console.log(id == ida)
+
+//for using big number, place n at the end of the number
+//const bigNumber = 211211212122112211212n
+
+
+//Array, Objects, Functions
+const heroes =["a","b","c"]
+let obj = {
+    name:"hj",
+    age:22
+}
+const myFunction = function (){
+console.log("hi")
+}
+
+myFunction()
+
+//to know the datatype, use
+// typeof
+// console.log(typeof variablename)
