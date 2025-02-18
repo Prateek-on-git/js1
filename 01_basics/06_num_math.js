637294fba49b3c1b9f2bbcd0e0a30958aba1f3d1
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-02-18 14:04:29 +0000

    num1

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,11 @@
+const score = 400
+const balance = new Number(100.56)
+
+console.log(balance)
+console.log(score)
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+//use tolocalstring('en-In') for displaying commas in the big values for Indian Rupees
+
