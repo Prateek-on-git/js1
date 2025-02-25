6358b39deb18711b5db8f9268518691dc52918d9
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-02-25 09:34:30 +0000

    obj2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,33 @@
+const u1 = {}
+
+u1.id="123"
+u1.name = "names"
+u1.loggedin =false
+
+console.log(u1)
+
+const regu1 ={
+    email:"a@e.com",
+   totname:{ fullname:{
+        fname:"First",
+        sname:"last"
+    }
+}
+
+}
+console.log(regu1.totname.fullname.sname);
+const obj1 ={
+    1:"a",
+    2:"b"
+}
+const obj2 ={
+    3:"c",
+    4:"d"
+}
+// const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3)
+
+//hasown property to check if the object has the defined key or not
