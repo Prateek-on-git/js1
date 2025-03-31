d146b162e607ef127ca938823ae76cf9707c56a5
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-03-31 07:03:29 +0000

    hi

--- 04_Control_flow/one.js
+++ 04_Control_flow/one.js
@@ -25,3 +25,22 @@ switch (month) {
         break;
 }
  
+
+//falsy values 
+// false 0, -0, BigInt 0n, "", null,
+//undefined //
+
+//truthy values
+// "0",'false', " " -string values , {},[],
+//function (){}
+
+
+const emptyObj= {}
+if(Object.keys(emptyObj).length===0){
+    console.log("Object is empty")
+}
+userEmail =[]
+
+if (userEmail.length===0){
+    console.log("Array is Empty");
+}
