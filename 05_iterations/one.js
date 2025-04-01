d765c0a1d583b6f4844a777277afb76a3654d0b6
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-04-01 12:40:22 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,7 @@
+
+array = [1,2,3,4,5,5]
+console.log(array.length)
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(`${element} & ${index}`)
+}
