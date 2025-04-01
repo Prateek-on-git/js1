35cdcd2c890ac4811d0252810295b8ba765b2282
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-04-01 12:56:11 +0000

    sd

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,7 +1,16 @@
 
-array = [1,2,3,4,5,5]
+array = [1,2,3,4,5,6]
 console.log(array.length)
 for (let mew = 0; mew < array.length; mew++) {
     const element = array[mew];
-    console.log(`${element} & ${mew}`)
+    if(mew==3){
+        //console.log("3 number")
+    }
+   // console.log(`${element} & ${mew}`)
+}
+
+
+for (let   sdd = 0;   sdd < array.length;   sdd++) {
+    const element = array[  sdd];
+    
 }
