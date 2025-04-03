bcea9f1592b747344eaa2de6385335c0a9383b40
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-04-03 11:06:16 +0000

    for of

---
+++ 05_iterations/three.js
@@ -0,0 +1,14 @@
+// for of loop -high order array loop
+
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings ="Hello wword"
+// for (const num of greetings) {
+//     console.log(`Hi ${num}`)
+    
+// }
+
+//Maps
