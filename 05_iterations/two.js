43081003800b605bf5b4c27ec2c41c4fc93ed0ef
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-04-03 10:54:00 +0000

    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+i=5;
+
+// while (i<10) {
+//     console.log(`Value of ${i}`)
+//     i++
+// }
+
+let arrayar =[1,2,3,4]
+
+
+// while (arr<arrayar.length) {
+//     console.log(arrayar[arr])
+//     arr = arr+1
+
+// }
+
+let score = 13;
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score<=10);
