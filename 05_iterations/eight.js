8482365ea3d982ee9f2db9e58f59cf9f8728965d
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-04-07 13:59:16 +0000

    reduce function

---
+++ 05_iterations/eight.js
@@ -0,0 +1,10 @@
+//Reduce function 
+
+const myNumbers = [1,2,3,4,5,6,7,8]
+
+const initalValue = 0;
+const Reducefunc = myNumbers.reduce(
+    (accumulator,curentValue)=>accumulator+curentValue,initalValue
+)
+console.log(Reducefunc);
+
