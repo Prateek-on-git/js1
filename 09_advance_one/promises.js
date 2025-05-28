3928ac7c4e7f1abe6e7f7550509127f170314684
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-05-28 12:57:14 +0000

    promis123

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,1 +1,31 @@
-const promiseOne = new Promise()
+const promiseOne = new Promise(function(resolve,reject){
+    // Do an async task
+    //DB Calls, Cryptography, network calls
+    setTimeout(function(){
+        console.log("Async task is complete")
+        resolve()
+    },1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed")
+})
+
+
+
+
+new Promise(function(resolve,reject){
+setTimeout(function(){
+    console.log("Async Task2");
+    resolve()
+},1000)
+
+}).then(function(){
+    console.log("async2 resolved")
+})
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        
+    },1000)
+})
