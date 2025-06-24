f8ec5394419ddb43798a8a07c1edb10fd59ce6b6
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-06-24 11:54:16 +0000

    obj

---
+++ 10_classes_and_oop/Object.js
@@ -0,0 +1,16 @@
+function multiby5(num){
+    return num*5
+    console.log(num)
+}
+
+multiby5.power = 4
+console.log(multiby5(5))
+
+console.log(multiby5.power);
+console.log(multiby5.prototype);
+
+function createUser(username,score){
+    this.username = username
+    this.score = score``
+
+}
