956d71ad025be15503de5a423aa7fb66ff591c06
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-06-25 09:17:39 +0000

    new obj1

--- 10_classes_and_oop/Object.js
+++ 10_classes_and_oop/Object.js
@@ -11,6 +11,23 @@ console.log(multiby5.prototype);
 
 function createUser(username,score){
     this.username = username
-    this.score = score``
+    this.score = score
 
 }
+
+createUser.prototype.increment = function(){
+    this.score++
+    console.log(this.score)
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`score is ${this.score}`);
+}
+
+const chai = new createUser("chai",25)
+const tea = new createUser("tea",40)
+
+``
+
+chai.increment()
+chai.printMe()
