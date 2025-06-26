939a53138588f4c4873db0f25e3d8ae281cf0b4b
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-06-26 12:02:07 +0000

    tea

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,31 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`username is ${this.username}`);
+        
+    }
+
+}
+
+class Teacher extends User{
+    constructor(username,email,password){
+    super(username)
+    this.email = email
+    this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was ${this.username}`);
+        
+    }
+    
+}
+
+const chai = new Teacher("chai","a@a.com",123)
+chai.addCourse()
+
+const tea = new User("tea")
+tea.logMe()
