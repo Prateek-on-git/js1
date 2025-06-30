d298e1a1c588773c9bec4b5de18c226f4e08b0a1
Prateek-on-git <155351382+Prateek-on-git@users.noreply.github.com>
2025-06-30 12:01:27 +0000

    obkect.js

---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,19 @@
+const User = {
+    _email: 'A@a.com',
+    _password: '1234',
+
+
+    get email(){
+        return this.email.toUppercase
+    },
+
+    set email(value)
+    {
+     this._email = value   
+    }
+
+
+
+
+}
+
